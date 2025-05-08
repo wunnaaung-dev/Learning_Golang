6ca808739724aa28f12a9eb3f3cc997892c95eb4
Wunna Aung <wunnaaung_cool_guy_lay@OnEarth.localdomain>
2025-05-09 02:07:07 +0700

    api with go

--- build-crud-app-golang/main.go
+++ build-crud-app-golang/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -49,6 +48,31 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	for _, item := range movies {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index + 1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
 			return
 		}
 	}
